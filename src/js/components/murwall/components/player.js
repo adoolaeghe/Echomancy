6f9d48f0dd4fd76f7eaa8ab27e8a2030b0677d67
Antoine Doolaeghe <antoinedoolaeghe@gmail.com>
2018-01-24 08:19:24 +0000

    It trigger the play click

--- src/js/components/murwall/components/player.js
+++ src/js/components/murwall/components/player.js
@@ -3,11 +3,38 @@ import PieChart from "../../player/components/PieChart/pieChart";
 
 export default class Player extends React.Component {
 
+  constructor() {
+    super()
+    this.state = {
+      style: {}
+    }
+  }
+
+  handlePlayClick() {
+    this.props.handleOnOff(this.props.uKey);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.onKey === this.props.uKey) {
+      this.setState({
+        style: {
+          backgroundColor: "red"
+        }
+      })
+    } else {
+      this.setState({
+        style: {}
+      })
+    }
+  }
+
   render() {
     return (
       <div className="player-main-wrapper col xl3 l4 m6 s12">
         <PieChart />
-        <div className="player-cover"></div>
+        <div className="player-cover"
+             style={this.state.style}
+             onClick={this.handlePlayClick.bind(this)}></div>
         <div>
           <div className="player-song-name row">
             <p className="col s12">Song Name</p>
--- src/js/components/murwall/murwall.js
+++ src/js/components/murwall/murwall.js
@@ -2,10 +2,29 @@ import React from "react";
 import Player from "./components/player"
 export default class MurWall extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      on : null
+    }
+  }
+
+  handleOnOff(playerKey) {
+    this.setState({
+      on : playerKey
+    })
+  }
+
   handlePlayerRender() {
     let to_return = [];
     for(let i = 0; i < this.props.players; i++) {
-      to_return.push(<Player />)
+      to_return.push(
+        <Player key={i}
+                uKey={i}
+                onKey={this.state.on}
+                handleOnOff={this.handleOnOff.bind(this)}
+        />
+      )
     }
     return to_return;
   }
