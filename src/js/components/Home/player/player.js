66fc66edc1f619eca9df69b2670f4e3cd6040485
adoolaeghe <antoine.doolaeghe@draysontechnologies.com>
2018-03-04 12:06:17 +0000

    it can play song

--- src/js/components/Home/player/player.js
+++ src/js/components/Home/player/player.js
@@ -3,19 +3,40 @@ import React from "react";
 import Palette from "./components/palette/Palette";
 import PieChart from "./components/PieChart/pieChart";
 import FloatingActionButton from 'material-ui/FloatingActionButton';
+var createPlayer = require('web-audio-player')
+createPlayer.crossOrigin = "anonymous";
 
 export default class Player extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      playing: "pause",
+      audio: createPlayer('public/content/images/songs/daft_punk.mp3')
+    };
   }
 
+  handlePlayClick() {
+    if(this.state.playing === "pause"){
+      this.setState({
+        playing: "playing",
+      })
+      this.state.audio.play();
+      this.state.audio.node.connect(this.state.audio.context.destination)
+    } else {
+      this.setState({
+        playing: "pause"
+      })
+      this.state.audio.pause();
+    }
+  }
+  
   render() {
     return (
       <div className="player player-wrapper">
         <PieChart />
-        <div className="player-cover large">
-          <div className={`player-btn-icon`}></div>
+        <div className="player-cover large"
+             onClick={() => {this.handlePlayClick()}}>
+          <div className={`player-btn-icon ${this.state.playing}`}></div>
         </div>
       </div>
     );
